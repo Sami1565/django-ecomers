7227912a559f61c7088493ab7e119dc7eddb8777
Syed Samad <syedsamad1565@gmail.com>
2026-07-23 15:24:47 +0500

    Added e-commerce product models and views

--- store/models.py
+++ store/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def in_stock(self):
+        return self.stock > 0
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+        # Add these methods to your models
+
+class Category(models.Model):
+    # ... existing code ...
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('store:product_list_by_category', args=[self.slug])
+
+class Product(models.Model):
+    # ... existing code ...
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('store:product_detail', args=[self.slug])
--- store/views.py
+++ store/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Product, Category
 
-def home(request):
-    return HttpResponse("<h1 style='color:blue;'>Hello World! Django E-Commerce</h1>")
+def product_list(request, category_slug=None):
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    else:
+        category = None
+    
+    # Pagination (12 products per page)
+    paginator = Paginator(products, 12)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    }
+    return render(request, 'store/product_list.html', context)
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    context = {'product': product}
+    return render(request, 'store/product_detail.html', context)
